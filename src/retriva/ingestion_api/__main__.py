ec3434bb86ee554e55ec4a99210d0d9375188709
Andrea Marson <am.dev.75@gmail.com>
2026-04-12 16:58:55 +0200

    Use global setting.

--- src/retriva/ingestion_api/__main__.py
+++ src/retriva/ingestion_api/__main__.py
@@ -10,7 +10,7 @@ def main():
     print(f"##### Retriva RAG backend ({config.VERSION}) #####\n")
     parser = argparse.ArgumentParser(description="Retriva RAG backend")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="Binding host")
-    parser.add_argument("--port", type=int, default=8000, help="Binding port")
+    parser.add_argument("--port", type=int, default=config.settings.ingestion_api_port, help="Binding port (default: %(default)s)")
     args = parser.parse_args()
     
     print(f"Starting API server on {args.host}:{args.port}...")
